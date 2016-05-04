2fbfff924667b570529a440d5e8b16d13f5e548d
PalermoD <palermo.deschamps@gmail.com>
2016-05-04 11:51:43 -0400

    angular todos

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-angular.module("angularToDo", []);
+angular.module("angularTodo", [])
+   .controller('mainCtrl', function($scope) {
+  	$scope.helloWorld = function(){
+  		console.log("hello there");
+  	};
+
+  	$scope.todos = [
+       {"name": "clean room"},
+       {"name": "finish app"},
+       {"name": "do laundry"}
+     ]
+ });
---
+++ scripts/hello-world.js
@@ -0,0 +1,7 @@
+angular.module('angularTodo')
+   .directive('helloWorld', function(){
+   	 return {
+   	 	template: "This is a hello world directive"
+   	 	
+   	 }
+   })
